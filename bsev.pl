e68e1b6218e0f6ac8aa1235c47b2d616268f9db6
osfameron <hakim.cassimally@gmail.com>
2016-04-16 18:16:17 +0000

    Initial version of demo code in Perl
    NB: warnings shown as follows:
    
        18:16 $ perl bsev.pl
        The signatures feature is experimental at bsev.pl line 51.
        The lexical_subs feature is experimental at bsev.pl line 52.
        The signatures feature is experimental at bsev.pl line 52.
        The signatures feature is experimental at bsev.pl line 72.
        bless( {
          id => "hignfy1"
        }, 'Episode' )
        bless( {
          id => "hignfy"
        }, 'Series' )
        bless( {
          id => "bbc"
        }, 'Brand' )
    
        18:17 $ perl -v
        This is perl 5, version 22, subversion 0 (v5.22.0) built for x86_64-linux
    
    If `no warnings` is moved to within the package (e.g. where it is currently
    commented out) then the warnings are quelled as expected

---
+++ bsev.pl
@@ -0,0 +1,103 @@
+use feature 'signatures';
+use feature 'lexical_subs';
+no warnings "experimental::signatures";
+no warnings "experimental::lexical_subs";
+
+package Programme;
+use Moo;
+use Types::Standard qw/ Maybe Str /;
+
+has id => ( is => 'ro', isa => Maybe[Str] );
+has id2 => ( is => 'ro', isa => Maybe[Str] );
+
+package Brand;
+use Moo;
+extends 'Programme';
+
+package Series;
+use Moo;
+extends 'Programme';
+
+package Episode;
+use Moo;
+extends 'Programme';
+
+package Database;
+use Moo;
+use MooX::HandlesVia;
+use Types::Standard qw/ ArrayRef HashRef InstanceOf /;
+
+# no warnings "experimental::signatures";
+# no warnings "experimental::lexical_subs";
+
+has list => (
+    is => 'ro',
+    isa => ArrayRef[InstanceOf["Programme"]],
+    handles_via => 'Array',
+    handles => {
+        filter => 'grep',
+    }
+);
+
+has graph => (
+    is => 'ro',
+    isa => HashRef[InstanceOf["Programme"]],
+    handles_via => 'Hash',
+    handles => {
+        lookup => 'get'
+    }
+);
+
+sub search ($self, $prog1) {
+    my sub match ($prog2) {
+        return unless ref $prog1 eq ref $prog2;
+
+        return unless $prog1->id or $prog1->id2;
+        return unless $prog2->id or $prog2->id2;
+        
+        return unless $prog1->id or $prog2->id2;
+        return unless $prog1->id2 or $prog2->id;
+
+        return if $prog1->id and $prog2->id 
+            and $prog1->id ne $prog2->id;
+        return if $prog1->id2 and $prog2->id2 
+            and $prog1->id2 ne $prog2->id2;
+
+        return 1;
+    }
+
+    return $self->filter(\&match);
+}
+
+sub getHierarchy($self, $prog) {
+    if (my $parent = $self->lookup($prog->id)) {
+        return ($prog, $self->getHierarchy($parent))
+    }
+    else {
+        return $prog;
+    }
+
+}
+
+package main;
+use Data::Dumper::Concise;
+
+my $b1 = Brand->new( id => "bbc" );
+my $s1 = Series->new( id => "hignfy");
+my $e1 = Episode->new( id => "hignfy1");
+
+my $db = Database->new(
+    list => [$b1, $s1, $e1],
+    graph => {
+        $e1->id => $s1,
+        $s1->id => $b1,
+    }
+);
+
+print Dumper(
+    $db->getHierarchy(
+        $db->search(
+            Episode->new( id => 'hignfy1' )
+        )
+    )
+);
